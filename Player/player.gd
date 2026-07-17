b04abb8bc04f0c38a06a2477b7e49a96a7d87738
Omarfadalla <omarfadalla@gmail.com>
2026-07-18 02:32:30 +0300

    added basic level scene

--- Player/player.gd
+++ Player/player.gd
@@ -2,10 +2,10 @@ extends CharacterBody2D
 
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * 300
+	velocity = direction * 100 
 	
 	if velocity.length() > 0 :
 		play_walk_animation()
