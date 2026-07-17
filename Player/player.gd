e76c515881e7352edc672f5524cfc10a295e5bf5
Omarfadalla <omarfadalla@gmail.com>
2026-07-18 02:12:17 +0300

    added movement animations

--- Player/player.gd
+++ Player/player.gd
@@ -1,10 +1,28 @@
 extends CharacterBody2D
 
 
+
 func _physics_process(delta: float) -> void:
 	
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = direction * 300
-	move_and_slide()
+	
+	if velocity.length() > 0 :
+		play_walk_animation()
+	else:
+		play_idle_animation()
+
+
+
+
+	move_and_slide() 
+	pass
+
+func play_idle_animation():
+	$AnimatedSprite2D.play("idle")
+	pass
+
+func play_walk_animation():
+	$AnimatedSprite2D.play("walk")
 	pass
 	
