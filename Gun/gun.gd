6c09d091dd9cb34dd0efc1a137e6365ba4c305d3
Omarfadalla <omarfadalla@gmail.com>
2026-07-18 16:20:37 +0300

    added basic bullet scripting

--- Gun/gun.gd
+++ Gun/gun.gd
@@ -1,9 +1,15 @@
 extends Area2D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
 	pass
+	
+func Shoot():
+	const BULLET = preload("res://bullet/bullet.tscn")
+	var new_bullet  = BULLET.instantiate()
+	new_bullet.global_position = %ShootPoint.global_position
+	%ShootPoint.add_child(new_bullet)
--- bullet.gd
+++ bullet.gd
@@ -1,9 +1,23 @@
 extends Area2D
 
+var travel_distance = 0
 
 func _physics_process(delta: float) -> void:
+	const Speed = 1000
+	const RANGE = 1200
 	
 	var direction  = Vector2.RIGHT.rotated(rotation)
-	position += direction * 1200 * delta
+	position += direction * Speed * delta
 	
+	travel_distance += Speed * delta
+	if travel_distance > RANGE:
+		queue_free()
+	 
 	pass
+
+
+func _on_body_entered(body) -> void:
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
+	pass # Replace with function body.
