360159b47b462b45b399ad828a74c1982d7378bb
Omarfadalla <omarfadalla@gmail.com>
2026-07-18 03:13:26 +0300

    forgot to add wakatime extension ! :((

--- slime_mob.gd
+++ slime_mob.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
 var player 
+func Play_walk():
+	$AnimatedSprite2D.play("walk")
+	
+func Play_hurt():
+	$AnimatedSprite2D.play("hurt")
+	$AnimatedSprite2D.queue("walk")
 
 func _ready() -> void:
 	player = get_node("/root/Game/Player")
 	pass
-func _physics_process(delta: float) -> void:
+
+func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 20
+	Play_walk()
 	move_and_slide() 
 	pass
-
-func Play_walk():
-	$CharacterBody2D/AnimatedSprite2D.play("walk")
-	
-	
-func Play_hurt():
-	$CharacterBody2D/AnimatedSprite2D.play("hurt")
-	$CharacterBody2D/AnimatedSprite2D.queue("walk")
