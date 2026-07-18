289650b79745885cd267b7aa15d58d3087d2682b
Omarfadalla <omarfadalla@gmail.com>
2026-07-18 18:42:23 +0300

    added mod spawning

--- slime_mob.gd
+++ slime_mob.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 20
+	velocity = direction * 100
 	move_and_slide() 
 	pass
 
